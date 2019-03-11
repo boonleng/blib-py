bd72d0d365ae83d0e99458a2774de34d54139321
boonleng <boonleng@ou.edu>
2019-03-11 15:49:06 -0500

    Updated python_profile

--- python_profile.py
+++ python_profile.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
+#
+# Recommended usage:
+#
+# 1) Copy this file to ~/.python_profile
+# 2) Add the following line to ~/.bash_profile
+#
+#    export PYTHONSTARTUP=${HOME}/.python_profile
+#
+# 3) Restart the terminal for 2) to take effect
+#
 import platform
 hostname = platform.node()
 if hostname == 'dawn':
@@ -13,9 +23,34 @@ sys.ps1='\033[38;5;{}m>\033[38;5;{}m>\033[38;5;{}m>\033[0m '.format(c[0], c[1], 
 sys.ps2='\033[38;5;214m...\033[0m '
 
 import matplotlib
-matplotlib.rcParams['font.family'] = 'serif'
-matplotlib.rcParams['font.serif'] = ['Arial', 'Helvetica']
-matplotlib.rcParams['font.sans-serif'] = ['System Font', 'Verdana', 'Arial']
-matplotlib.rcParams['figure.figsize'] = (8.89, 5)   # Change the size of plots
 matplotlib.rcParams['figure.dpi'] = 144
-
+matplotlib.rcParams['font.family'] = ['serif']
+matplotlib.rcParams['font.sans-serif'] = ['System Font',
+                                          'Arial',
+                                          'DejaVu Sans',
+                                          'Bitstream Vera Sans',
+                                          'Computer Modern Sans Serif',
+                                          'Lucida Grande',
+                                          'Verdana',
+                                          'Geneva',
+                                          'Lucid',
+                                          'Arial',
+                                          'Helvetica',
+                                          'Avant Garde',
+                                          'sans-serif']
+matplotlib.rcParams['font.serif'] = ['Arial', 
+                                     'DejaVu Serif',
+                                     'Bitstream Vera Serif',
+                                     'Computer Modern Roman',
+                                     'New Century Schoolbook',
+                                     'Century Schoolbook L',
+                                     'Utopia',
+                                     'ITC Bookman',
+                                     'Bookman',
+                                     'Nimbus Roman No9 L',
+                                     'Times New Roman',
+                                     'Times',
+                                     'Palatino',
+                                     'Charter',
+                                     'serif']
+matplotlib.rcParams['text.usetex'] = False
