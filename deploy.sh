cc96c9d5000e0ef675f155650ecdaac888d57b8f
boonleng <boonleng@ou.edu>
2023-12-04 22:03:56 -0600

    Adopted pyproject.toml

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-python setup.py sdist
-twine upload --verbose --repository blib-py $(ls -t dist/* | head -n 1)
+python -m build --no-isolation
+
+version=$(grep "__version__" src/blib/__init__.py | awk -F'"' '{print $2}')
+archive=$(ls -t dist/blib-py-${version}.tar.gz)
+
+echo "version = ${version}   archive = ${archive}"
+
+twine upload --verbose --repository blib-py ${archive}
