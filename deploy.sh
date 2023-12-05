f2d2d91def475752db074bdca74a5c7afeb99fff
boonleng <boonleng@ou.edu>
2023-12-05 08:12:00 -0600

    Configured to be more specific

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python -m build --no-isolation
+python -m build
 
 version=$(grep "__version__" src/blib/__init__.py | awk -F'"' '{print $2}')
 archive=$(ls -t dist/blib-py-${version}.tar.gz)
 
 echo "version = ${version}   archive = ${archive}"
 
-twine upload --verbose --repository blib-py ${archive}
+python -m twine upload --verbose --repository blib-py ${archive}
