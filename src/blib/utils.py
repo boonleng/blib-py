d856c666a02a39f1fe915b400706a2a015dd596a
boonleng <boonleng@ou.edu>
2024-02-01 16:43:42 -0600

    Minor cosmetic adjustments

--- src/blib/utils.py
+++ src/blib/utils.py
@@ -286,9 +286,12 @@ def cplot(t, x=None):
     if x is None:
         x = t
         t = np.arange(len(t))
+    a = np.abs(x)
+    m = np.max(a) * 1.3
     h1 = plt.plot(t, x.real, label="I", zorder=4)
     h2 = plt.plot(t, x.imag, label="Q", zorder=3)
-    h3 = plt.plot(t, np.abs(x), label="A", linewidth=0.8, zorder=2)
-    plt.legend()
+    h3 = plt.plot(t, a, label="A", linewidth=0.8, zorder=2)
+    plt.legend(loc="lower left", ncol=3)
+    plt.gca().set(ylim=[-m, m])
     plt.grid()
     return [*h1, *h2, *h3]
