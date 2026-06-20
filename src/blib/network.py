3ec0d7f98b61b91ba696afc7452e3c748d361444
boonleng <boonleng@ou.edu>
2026-06-20 13:42:42 -0500

    Indicate download location

--- src/blib/network.py
+++ src/blib/network.py
@@ -46,7 +46,7 @@ def get_ip_location(ip, show_city=False, abbreviate=False, download=True):
     if ip_location_db_fid is None:
         dbs = sorted(glob.glob(f"{base_dir}/*.mmdb"))
         if len(dbs) == 0 and download:
-            print("Downloading IP location database...")
+            print(f"Downloading IP location database to {base_dir}...")
             basename = os.path.basename(database_download_url)
             database = os.path.join(base_dir, basename)
             urllib.request.urlretrieve(database_download_url, database)
